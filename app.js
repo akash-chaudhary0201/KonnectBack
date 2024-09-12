ad9d88a9ba850e1f703fd7a4763eceeafd617fa5
Akash Chaudhary <akashchaudhary0201@gmail.com>
2024-09-12 23:22:32 +0530

    changed cors policy to all

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+const express = require("express");
+const app = express();
 const cors = require("cors");
 const cookierParser = require("cookie-parser");
-
 app.use(cookierParser());
 const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 
 const corsOption = {
-  origin: "*", // Allows requests from any origin
+  origin: "*",
   credentials: true,
 };
 
