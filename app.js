ed9fcb82155392d2bf8fd397dced3d6f9fe1b50f
Akash Chaudhary <akashchaudhary0201@gmail.com>
2024-09-14 19:09:28 +0530

    A

--- app.js
+++ app.js
@@ -7,21 +7,11 @@ const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 
-const allowedOrigins = [
-  "https://konnect-front.vercel.app",
-  "http://localhost:5173/",
-];
 const corsOption = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "https://konnect-front.vercel.app",
   credentials: true,
 };
+
 app.use(cors(corsOption));
 
 // importing all routes :-
